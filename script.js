72ebb6cea6ade73467de139a7104dd3c836c69da
amygemme81 <domathlikearockstar@gmail.com>
2019-05-12 08:45:02 -0700

    I set the min values for the inputs so users can't enter negative numbers or split the check between 0 people.  I added some style changes as well

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 
 function displaytip(val) {
     let bill = grabBill();
+    console.log(document.getElementById('percent').value, "default percent");
+    if(val){
     document.getElementById('percent').value = val;
+    }
+    else{
+        val=document.getElementById('percent').value
+    }
     console.log(bill, "bill");
     let scale = val / 1;
     console.log(scale, "scale")
@@ -12,7 +18,7 @@ function displaytip(val) {
     console.log(amount, "amount")
     let num = grabNum();
     console.log(num, "num");
-    let tip = amount / num;
+    let tip = (amount / num).toFixed(2);
     console.log(tip, "tip")
     $("#amount").text(tip);
     $("#percent").text(scale);
